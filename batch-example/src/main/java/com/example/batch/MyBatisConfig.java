52665a363fc2f2d434cdcfc5615711a332a16be1
maenodaisuke <developer.daisuke.maeno@gmail.com>
2017-08-13 23:23:29 +0900

    Change a scope of postgresql from compile to runtime.

--- batch-example/src/main/java/com/example/batch/MyBatisConfig.java
+++ batch-example/src/main/java/com/example/batch/MyBatisConfig.java
@@ -2,7 +2,6 @@ package com.example.batch;
 
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.postgresql.Driver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
@@ -15,16 +14,16 @@ import javax.sql.DataSource;
 public class MyBatisConfig {
 
     @Bean
-    public SqlSessionFactoryBean sqlSessionFactoryBean() {
+    public SqlSessionFactoryBean sqlSessionFactoryBean() throws ClassNotFoundException {
         final SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         sqlSessionFactoryBean.setDataSource(datasource());
         return sqlSessionFactoryBean;
     }
 
     @Bean
-    public DataSource datasource() {
+    public DataSource datasource() throws ClassNotFoundException {
         final SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        dataSource.setDriverClass(Driver.class);
+        dataSource.setDriverClass((Class<? extends java.sql.Driver>) Class.forName("org.postgresql.Driver"));
         dataSource.setUrl("jdbc:postgresql://localhost:5432/");
         dataSource.setUsername("scott");
         dataSource.setPassword("tiger");
